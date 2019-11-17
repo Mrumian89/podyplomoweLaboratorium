a7b617296d017184e72e78c1e892d0a5c5e23796
Małgorzata Rumian <mrumian@student.wszib.edu.pl>
2019-11-17 08:43:23 +0100

    Laboratorium 1

---
+++ src/com/company/Podyplomowe.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Podyplomowe {
+    public static void main(String[] args) {
+        int a = 2;
+        char b = 'A';
+
+        System.out.println("\nNumer " + a + b);
+
+
+    }
+}
